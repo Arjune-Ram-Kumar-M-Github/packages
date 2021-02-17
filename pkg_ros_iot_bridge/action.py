c3ddfb0d85c506b1f36deac2f28ecd5446a7629e
Arjune Ram Kumar <arjune@student.tce.edu>
2021-02-17 19:16:30 +0530

    commit

---
+++ pkg_ros_iot_bridge/action.py
@@ -0,0 +1,21 @@
+
+'''
+ROS-IoT-Action File
+
+Goal Type - String
+::
+
+    # goal
+    string message
+    ---
+
+    Result Type - Bool
+    # result
+    bool flag_success
+    ---
+
+    Feedback Type - int8
+    # feedback
+    int8 percentage_complete
+
+'''
---
+++ pkg_ros_iot_bridge/sub_mqtt_msg.py
@@ -0,0 +1,14 @@
+'''
+MQTT Subscription Message File
+
+* ``timestamp`` - type : Time
+* ``topic`` - type : string
+* ``messages`` - type : string
+::
+
+    time timestamp 
+    string topic
+    string message
+
+
+'''
